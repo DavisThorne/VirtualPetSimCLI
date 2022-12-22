9faced5a5df645ad888fe7f0bf483b745c88c516
Davis <61391491+dskmgmt@users.noreply.github.com>
2022-12-22 16:30:08 +0000

    Added New Classes: LoadGame.cs, MainGame.cs, SaveGame,cs

---
+++ VirtualPetSimCLI/src/main/SaveGame.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace VirtualPetSimCLI.main;
+
+public abstract class SaveGame
+{
+    public static void Save()
+    {
+        var fullFilePath = SaveData.FullFilePath;
+        using (StreamWriter file = new StreamWriter(File.Open(fullFilePath, FileMode.Open)))
+        {
+            var jObject = JObject.FromObject(new
+            {
+                SaveData.CharacterName,
+                SaveData.PetName,
+                SaveData.PetSpecies,
+                SaveData.PetAge,
+                SaveData.PetHunger,
+                SaveData.PetThirst,
+                SaveData.PetHappiness,
+                SaveData.PetHealth,
+                SaveData.PetEnergy,
+                SaveData.PetCleanliness
+            });
+            string jsonSaveData = JsonConvert.SerializeObject(jObject, Formatting.Indented);
+            file.Write(jsonSaveData);
+        }
+    }
+}
